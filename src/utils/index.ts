15eccc6f738f24b4f5c6740cb1559ffc2a914276
Сайд-Али Орзамиев <obkh1309@mail.ru>
2023-08-12 14:37:41 +0300

    third commit in Project

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,15 @@
+const PRESSURE_UNITS = 0.750062;
+
 export const capitalizeFirstLetter = (str: string): string => {
     if(!str) return '';
 
     return `${str[0].toUpperCase()}${str.slice(1)}`
+}
+
+export const getPressureMm = (hpa: number): number => {
+    return Math.round(hpa * PRESSURE_UNITS);
+}
+
+export const getTime = (seconds: number): string => {
+    return new Date(seconds * 1000).toLocaleTimeString('ru-RU', { timeZone: 'Atlantic/Reykjavik' })
 }
